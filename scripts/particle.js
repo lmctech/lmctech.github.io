0c96d5363a88eb75d0f52880eeb10137126a07e6
lauranicaodh <lmchughtech@gmail.com>
2024-03-12 20:59:25 +0100

    March-2024-Update

--- scripts/particle.js
+++ scripts/particle.js
@@ -1,58 +1,107 @@
-var score=0;
+var score = 0;
+
+
 function Particle(x, y, length, maxSize, colour) {
-  var speed = 1;
+  this.velocity = createVector(random(-.1, .1), random(-.1, .1));
+  this.acceleration = createVector(random(-.1, .1), random(-.1, .1));
+  this.maxSpeed = 4;
   this.history = [];
-
-  let c = [color(252, 166, 18), color(255, 190, 11), color(255, 0, 110), color(131, 56, 236), color(58, 134, 255)];
-  this.x = x;
-  this.y = y;
+  this.pos = createVector(x, y);
+    
+  this.colours = [color(252, 166, 18), color(255, 190, 11), color(255, 0, 110), color(131, 56, 236), color(58, 134, 255)];
+  this.pos.x = x;
+  this.pos.y = y;
   this.length = length;
-  this.maxSize=maxSize;
+  this.maxSize = maxSize;
   this.size = maxSize;
-
+  
+  this.textmaxSize = 80;
   this.goalX = x;
   this.goalY = y;
-  this.d = map(dist(this.x, this.y, mouseX, mouseY), 0, 30, 0, 255);
-  this.c = c[colour % c.length];
-
-  this.update = function () {
-
-    var v = createVector(this.x, this.y);
+  this.d = map(dist(this.pos.x, this.pos.y, mouseX, mouseY), 0, 30, 0, 255);
+  this.cIndex = colour;
+  this.wIndex = 0;
+  this.c = this.colours[this.cIndex % this.colours.length];
+  this.control = 1;
+  
+  this.word = words[this.cIndex % words.length];
+  this.textC=255;
+  this.w = 0;
+  
+  
+  this.radius = this.size/2;
+  this.update = function () { 
+    this.c = this.colours[this.cIndex % this.colours.length];
+    var v = createVector(this.pos.x, this.pos.y);
     this.history.push(v);
     if (this.history.length > this.length) {
       this.history.splice(0, 1)
     }
   }
 
+  this.fixTextSize = function () {
+
+    if (page == "page_being.html") { 
+  
+      this.w = textWidth(this.word);
+
+      if( this.w > this.size){
+
+        if( this.w< this.textmaxSize ){
+          this.size=this.w +4;
+        }
+        else{
+          this.size=this.textmaxSize ;
+        }
+      }
+      
+    } 
+  }
   this.show = function () {
 
-    //trail 
     for (var i = 0; i < this.history.length; i++) {
       var pos = this.history[i];
       var r = map(i, 0, this.history.length, 4, this.size);
       this.alpha = map(i, 0, this.history.length, 0, 255);
       fill(this.c);
-      // fill(252, 166, 18,this.alpha);
       ellipse(pos.x, pos.y, r, r);
+
     }
 
-    // for (var i = 0; i < 20; i++)
-    // { 
-    //   var r2= i*this.maxSize;
+  }
+  this.showWords = function () {
 
-    //   this.alpha=map(i, 0, 10, 10, 0);
-    //   fill(252, 166, 18,this.alpha);
-    //   ellipse(this.x, this.y, r2, r2);
-    // }
+    //trail 
+    for (var i = 0; i < this.history.length; i++) { 
 
+   
+      var pos = this.history[i];
+      var r = map(i, 0, this.history.length, 4, this.size);
+      
+      this.alpha = map(i, 0, this.history.length, 0, 255);
+      fill(this.c);
+      // fill(252, 166, 18,this.alpha);
+      
+      stroke(this.c); 
+      ellipse(pos.x, pos.y, r, r);
+      noStroke();
+      if(i==this.history.length-1){
+        fill(this.textC);
+        textSize(14);
+        textAlign(CENTER, CENTER);
+        textWrap(WORD);
+        rectMode(CENTER);
+         text(this.word, pos.x, pos.y, this.textmaxSize-10,this.textmaxSize -10); 
 
+      }
+    }
   }
 
   this.follow = function () {
 
     //     this.goalX=mouseX;
     //     this.goalY=mouseY;
-    //     let d = map(dist(this.x, this.y, this.goalX, this.goalY), 1, 100, 0.02, 0.025);
+    //     let d = map(dist(this.pos.x, this.pos.y, this.goalX, this.goalY), 1, 100, 0.02, 0.025);
 
     //     if(this.fc==null){
     //       this.fc=60;
@@ -65,66 +114,162 @@ function Particle(x, y, length, maxSize, colour) {
     //     this.goalX=this.diffX+mouseX;
     //     this.goalY=this.diffY+mouseY;
 
-    //      d = map(dist(this.x, this.y, this.goalX, this.goalY), 1, 100, 0.02, 0.025);
-    let d = map(dist(this.x, this.y, mouseX, mouseY), 1, 100, 0.02, 0.1);
+    //      d = map(dist(this.pos.x, this.pos.y, this.goalX, this.goalY), 1, 100, 0.02, 0.025);
+    let d = map(dist(this.pos.x, this.pos.y, mouseX, mouseY), 1, 100, 0.02, 0.1);
+    d *= this.control;
 
-    this.x = lerp(this.x, mouseX, d);
-    this.y = lerp(this.y, mouseY, d);
+    this.pos.x = lerp(this.pos.x, mouseX, d);
+    this.pos.y = lerp(this.pos.y, mouseY, d);
   }
 
 
 
   this.wander = function () {
 
-    let d = dist(this.x, this.y, this.goalX, this.goalY);
+    let d = dist(this.pos.x, this.pos.y, this.goalX, this.goalY);
     //let dspeed = map(d, 0, width/2, 0.01, .02);
 
-    this.x = lerp(this.x, this.goalX, 0.01);
-    this.y = lerp(this.y, this.goalY, 0.01);
+    this.pos.x = lerp(this.pos.x, this.goalX, 0.01);
+    this.pos.y = lerp(this.pos.y, this.goalY, 0.01);
 
     if (d < 20) {
-      this.goalX = this.x + (random(-75, 75) * 2);
-      this.goalY = this.y + (random(-75, 75) * 2);
+      this.goalX = this.pos.x + (random(-75, 75) * 2);
+      this.goalY = this.pos.y + (random(-75, 75) * 2);
     }
 
 
-    let dn = dist(this.x, this.y, nemo.x, nemo.y);
+    let dn = dist(this.pos.x, this.pos.y, nemo.pos.x, nemo.pos.y);
 
-    if (dn < this.size*1.5) {
-      this.goalX = this.x + random(-150, 150);
-      this.goalY = this.y + random(-150, 150);
-      nemo.c = this.c;
+    if (dn < this.size * 1.5) {
+      this.goalX = this.pos.x + random(-150, 150);
+      this.goalY = this.pos.y + random(-150, 150);
+      nemo.cIndex = this.cIndex;
       nemo.size += 0.6;
       this.reBirth();
       score++;
+      if(!trackedWords.includes(this.word)){ 
+        addToBrain(this.word,this.cIndex);
+        trackedWords.push(this.word);
+      } 
     }
-    if(this.x<0 || this.goalX<0){
-      this.goalX=random(100);
+    if (this.pos.x < 0 || this.goalX < 0) {
+      this.goalX = random(100);
     }
-    if(this.y<0 || this.goalY<0){
-      this.goalY=random(100);
+    if (this.pos.y < 0 || this.goalY < 0) {
+      this.goalY = random(100);
     }
-    if(this.x>windowWidth || this.goalX>windowWidth){
-      this.goalX=windowWidth-random(100);
+    if (this.pos.x > windowWidth || this.goalX > windowWidth) {
+      this.goalX = windowWidth - random(100);
     }
-    if(this.y>windowHeight || this.goalY>windowHeight){
-      this.goalY=windowHeight-random(100);
+    if (this.pos.y > windowHeight || this.goalY > windowHeight) {
+      this.goalY = windowHeight - random(100);
     }
     this.grow();
   }
 
+  this.cluster = function(){
+     
+    let d = dist(this.pos.x, this.pos.y,   mazeX,mazeY);
+    this.goalX = lerp(this.goalX, mazeX , 0.02);
+    this.goalY = lerp(this.goalY, mazeY , 0.02);   
+    //let dspeed = map(d, 0, width/2, 0.01, .02);
+
+    this.pos.x = lerp(this.pos.x, this.goalX, 0.05);
+    this.pos.y = lerp(this.pos.y, this.goalY , 0.05);
+    if (d < 150) {
+      this.pos.x  +=  sin( (t* 0.02) +this.wIndex) ;
+      this.pos.y  += sin( (-t* 0.02) +this.wIndex) ;
+    }
+
+  } 
+  this.jiggle = function(){
+     
+
+    let d = dist(this.pos.x, this.pos.y,  nemo.pos.x, nemo.pos.y);
+    //let dspeed = map(d, 0, width/2, 0.01, .02);
+    if( d <= this.size *1.5 ){
+
+      this.goalX += random(-20, 20) ;
+      this.goalY += random(-20, 20) ; 
+    }
+  
+
+  }
+
   this.reBirth = function () {
-    this.size = 0;
-    this.x = random(windowWidth);
-    this.y = random(windowHeight);
-    this.goalX = this.x ;
-    this.goalY = this.y ;
-    
+    this.size = 0; 
+    this.pos.x = random(windowWidth);
+    this.pos.y = random(windowHeight);
+    this.goalX = this.pos.x;
+    this.goalY = this.pos.y;
+ 
   }
 
   this.grow = function () {
-    if (this.size < this.maxSize ) {
-      this.size +=0.1;
+    if (this.size < this.maxSize) {
+      this.size += 0.1;
+    }
+  }
+
+
+// collision code adapted from https://www.gorillasun.de/blog/an-algorithm-for-particle-systems-with-collisions/
+
+  this.updateState= function (newPos) {
+    
+    
+    this.velocity.add(this.acceleration);
+    this.velocity.limit(this.maxSpeed); // Limit the particle's speed
+    this.pos.add(this.velocity); 
+  }
+
+  this.checkEdges= function () {
+
+
+    this.radius = this.size/2;
+
+    if (this.pos.x - this.radius < 0) {
+      this.pos.x = this.radius; // Prevent from leaving the canvas from the left side
+      this.velocity.x *= -1;
+    } else if (this.pos.x + this.radius > windowWidth) {
+      this.pos.x = width - this.radius; // Prevent from leaving the canvas from the right side
+      this.velocity.x *= -1;
+    }
+
+    if (this.pos.y- this.radius < 0) {
+      this.pos.y= this.radius; // Prevent from leaving the canvas from the top
+      this.velocity.y *= -1;
+    } else if (this.pos.y+ this.radius > windowHeight) {
+      this.pos.y= height - this.radius; // Prevent from leaving the canvas from the bottom
+      this.velocity.y *= -1;
+    }
+  }
+  
+  this.checkCollision= function (){
+    for(let other of brain_bits){
+      if(this != other){
+        let distance = this.pos.dist(other.pos);
+        let minDistance = this.radius + other.radius;
+
+        
+        if(distance <= minDistance){
+           // Calculate collision response
+          let normal = p5.Vector.sub(other.pos, this.pos).normalize();
+          let relativeVelocity = p5.Vector.sub(other.velocity, this.velocity);
+          let impulse = p5.Vector.mult(normal, 2 * p5.Vector.dot(relativeVelocity, normal) / (1 + 1));
+
+          // Apply repulsion force to prevent sticking
+          let repulsion = p5.Vector.mult(normal, minDistance - distance);
+
+          // Update velocities
+          this.velocity.add(p5.Vector.div(impulse, 2));
+          other.velocity.sub(p5.Vector.div(impulse, 2));
+
+          // Apply repulsion force
+          this.pos.sub(p5.Vector.div(repulsion, 2));
+          other.pos.add(p5.Vector.div(repulsion, 2));
+      
+        }
+      }
     }
   }
 }
--- scripts/sketch.js
+++ scripts/sketch.js
@@ -3,60 +3,248 @@
 var x, y;
 var find;
 
+var page;
+var bg = 255;
 var nemo;
+var brain_bits = [];
 
 var npcs = [];
+let mazeLine;
+let colChange = 0;
+
+var mazeX, mazeY;
+
+// var words = ["hi","hello","sup","hey","yo","Bonjour", "Namaste", "Wassup", "Howzit", "Ciao",
+// "Salaam", "Hallo", "Shalom", "Hej", "Oi"];
+var words;
+
+function preload() {
+  img = loadImage('images/maze.svg');
+  words = loadStrings('list.txt');
+}
+
+var t;
+
 
 function setup() {
   let myCanvas = createCanvas(windowWidth, windowHeight);
   myCanvas.parent('background');
-  
+  placeMaze();
+  placeTracker();
+  mazeLine = createGraphics(windowWidth, windowHeight, P2D);
   x = 200;
   y = 200;
+  var path = window.location.pathname;
+  page = path.split("/").pop();
 
   noStroke();
   frameRate(60);
   find = false;
+  let psize = 15;
+  if (page == "page_being.html") {
+    psize = 40;
+  }
 
-  nemo = new Particle(300, 300,16,18,0)
+  nemo = new Particle(300, 300, 16, 18, 0);
   for (var i = 0; i < 15; i++) {
-    npcs[i] = new Particle(random(windowWidth), random(windowHeight), 16,10, i)
+
+    npcs[i] = new Particle(random(windowWidth), random(windowHeight), 16, psize, i);
+
+
   }
   frameRate(60);
 }
 
 function draw() {
+  t = frameCount;
+  background(bg);
 
-  background(255);
- 
+
+  if (page == "index.html") {
+    playMaze();
+  }
+
+  if (page == "index.html") {
+
+    imageMode(CORNER);
+    image(mazeLine, 0, 0);
+  }
   nemo.follow();
   nemo.update();
   nemo.show();
+  if (page == "index.html") {
+
+    imageMode(CENTER);
+    image(img, mazeX, mazeY);
+  }
 
   textSize(20);
   for (var i = 0; i < npcs.length; i++) {
 
     npcs[i].wander();
     npcs[i].update();
-    npcs[i].show();
-  }
-  if(score<5){
+    //npcs[i].show();
+    if (page == "page_being.html") {
 
-    
-    fill(0,map(score,1,5,0,100));
-    text('Score:'+score,50,windowHeight-50);
-  }
-  if(score>=5){
+      npcs[i].showWords();
+    } else {
 
-    fill(0,100);
-    text('Score:'+score,50,windowHeight-50);
+      npcs[i].show();
+    }
   }
-  if(score==69){
+  if (page == "page_being.html") {
+    showBrain();
+    if (t % tSpeed == 0) {
+      changeText();
+    }
+  }
 
-    fill(0,50);
-    text('Score:'+score+' (nice)',50,windowHeight-50);
+  
+  if (score < 5) {
+
+
+    fill(0, map(score, 1, 5, 0, 100));
+    text('Score:' + score, 50, windowHeight - 50);
+  }
+  if (score >= 5) {
+
+    fill(0, 100);
+    text('Score:' + score, 50, windowHeight - 50);
+  }
+  if (score == 69) {
+
+    fill(0, 50);
+    text('Score:' + score + ' (nice)', 50, windowHeight - 50);
   }
 }
+
+
+
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+  placeMaze();
+
 }
+
+function placeMaze() {
+
+  mazeX = (windowWidth * 0.6) + (img.width * .4);
+  mazeY = windowHeight / 2;
+
+}
+
+function playMaze() {
+  let dn = dist(mazeX, mazeY, nemo.pos.x, nemo.pos.y);
+
+  if (dn < img.width / 2) {
+    // fill(255,220,220);
+    //circle(mazeX ,mazeY, img.width  ); 
+    mazeLine.fill(nemo.c);
+    mazeLine.noStroke();
+    mazeLine.ellipse(nemo.pos.x, nemo.pos.y, 10, 10);
+    nemo.control = 2;
+
+    if (mouseIsPressed && millis() - colChange >= 500) {
+      nemo.cIndex++;
+      colChange = millis();
+    }
+  } else {
+    mazeLine.background(255, 255, 255, 5);
+    nemo.control = 1;
+  }
+}
+
+function placeTracker() {
+
+  trackerX = (windowWidth * 0.6) + (img.width * .4);
+  trackerY = windowHeight / 2;
+
+}
+var trackedWords = [" "];
+var trackedWordsCols = [0];
+
+function showBrain() {
+
+  for (let b of brain_bits) {
+    b.update();
+    b.checkCollision()
+    b.checkEdges();
+    b.updateState();
+    b.cluster();
+    fill(255);
+    b.showWords();
+    b.jiggle();
+  }
+}
+
+var braincounter = 0;
+
+function addToBrain(caughtWord, col) {
+
+
+  for (var i = 0; i <= braincounter; i++) {
+
+    brain_bits[braincounter] = new Particle(random(windowWidth), random(windowHeight), 16, 40, col);
+    brain_bits[braincounter].word = caughtWord;
+    brain_bits[braincounter].w = textWidth(caughtWord);
+    brain_bits[braincounter].goalX = mazeX;
+    brain_bits[braincounter].goalY = mazeY;
+
+    // if (brain_bits[braincounter].w > brain_bits[braincounter].size) {
+    //   brain_bits[braincounter].size = brain_bits[braincounter].w + 4;
+    // }
+    brain_bits[braincounter].fixTextSize();
+    brain_bits[braincounter].wIndex = braincounter;
+  }
+  console.log(brain_bits.length);
+  braincounter++;
+}
+
+var textInit = "Laura is a Stockholm-based creative technologist.";
+var textCurrent = "Laura is a Stockholm-based creative technologist.";
+var textTarget = "even more than the sum of these parts.";
+let phase = 0;
+let tSpeed=2;
+let tI=0;
+
+function changeText() {
+  var el = select("h1");
+  textCurrent = el.elt.textContent;
+
+  if (phase == 0) {
+    if (textCurrent != "Laura is") {
+
+      textCurrent = textCurrent.slice(0, textCurrent.length - 1);
+      
+    }
+    else{
+      phase=1;
+      tSpeed=20;
+    }
+  }
+
+  if (phase == 1) {
+      if (textCurrent != "Laura is...") {
+        textCurrent = textCurrent.concat('.');
+      }
+      else{
+        phase=2;
+        tSpeed=3;
+      }
+  }
+  if (phase == 2) {
+    if(score>4){
+      if (textCurrent != "Laura is...even more than the sum of these parts.") {
+        textCurrent = textCurrent.concat(textTarget.charAt(tI));
+        tI++;
+      }
+      else{ 
+      }
+    }
+    
+}
+
+
+    el.elt.textContent = textCurrent;
+
+  }
